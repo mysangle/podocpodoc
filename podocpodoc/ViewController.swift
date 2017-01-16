50e16537f92c93bb3d2d7eec4ce365190fcaf667
mysangle <mysangle@gmail.com>
2017-01-16 12:03:37 +0900

    Add initial version generated from xcode for project `podocpodoc`

---
+++ podocpodoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  podocpodoc
+//
+//  Created by soonhyung-imac on 1/16/17.
+//  Copyright © 2017 twentyhours. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
